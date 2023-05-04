5c9790bb372dc11ed3c018106563885bfe897773
Kelliton Santana <programmerKelliton@gmail.com>
2023-05-04 10:31:48 -0300

    Configurando ambiente

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "marine-blue": "hsl(213, 96%, 18%)",
+        "purplish-blue": "hsl(243, 100%, 62%)",
+        "pastel-blue": "hsl(228, 100%, 84%)",
+        "light-blue": "hsl(206, 94%, 87%)",
+        "strawberry-red": "hsl(354, 84%, 57%)",
+        "cool-gray": " hsl(231, 11%, 63%)",
+        "light-gray": " hsl(229, 24%, 87%)",
       },
+    },
+    fontWeight: {
+      normal: "400",
+      medium: "500",
+      bold: "700",
     },
   },
   plugins: [],
-}
+};
