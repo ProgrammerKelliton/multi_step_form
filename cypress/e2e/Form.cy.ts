e23b34fcb2dd04515c5013d2cd54179092d1227d
Kelliton Santana <programmerKelliton@gmail.com>
2023-05-06 15:14:00 -0300

    Adicionando teste em form

--- cypress/e2e/Form.cy.ts
+++ cypress/e2e/Form.cy.ts
@@ -1,43 +1,47 @@
-const BACKGROUND_STEP_IN_HIGHTLIGHT = ".bg-light-blue";
-const BACKGROUND_NEXT_BUTTON = ".bg-marine-blue";
-const TEXT_GOBACK_BUTTON = "Go Back";
+const STEP_IN_HIGHTLIGHT = ".bg-light-blue";
+const INPUT_PHONENUMBER = ":nth-child(3) > #input";
+const GOBACK_BUTTON = "Go Back";
+const NEXT_BUTTON = ".bg-marine-blue";
+const LABEL_ERROR = ".text-strawberry-red";
+const INPUT_EMAIL = ":nth-child(2) > #input";
+const INPUT_NAME = ":nth-child(1) > #input";
 
 function ClickOnNextButtonAndVerifyIfGoBackExists() {
   for (let index = 0; index !== 4; index++) {
-    cy.get(BACKGROUND_NEXT_BUTTON).click();
-    cy.contains(TEXT_GOBACK_BUTTON);
+    cy.get(NEXT_BUTTON).click();
+    cy.contains(GOBACK_BUTTON);
 
     // Verifica se existe um step em destaque
-    cy.get(BACKGROUND_STEP_IN_HIGHTLIGHT);
+    cy.get(STEP_IN_HIGHTLIGHT);
   }
 }
 
 function ClickOnGoBackButtonAndVerifyIfInStepOneItsHidden() {
   for (let index = 0; index !== 3; index++) {
-    cy.contains(TEXT_GOBACK_BUTTON).click();
-    cy.get(BACKGROUND_STEP_IN_HIGHTLIGHT);
+    cy.contains(GOBACK_BUTTON).click();
+    cy.get(STEP_IN_HIGHTLIGHT);
 
     // Verifica se existe um step em destaque
-    cy.get(BACKGROUND_NEXT_BUTTON);
+    cy.get(NEXT_BUTTON);
   }
 }
 
 function VerifyIfFormIsWorking() {
   // Name input
-  cy.get(":nth-child(1) > #input").type("Kelliton Santana de Souza");
+  cy.get(INPUT_NAME).type("Kelliton Santana de Souza");
 
   // Email input with error
-  cy.get(":nth-child(2) > #input").type("name example here");
-  cy.get(".text-strawberry-red");
+  cy.get(INPUT_EMAIL).type("name example here");
+  cy.get(LABEL_ERROR);
 
   // Email input without error
-  cy.get(":nth-child(2) > #input").clear();
-  cy.get(":nth-child(2) > #input").type("example@gmail.com");
+  cy.get(INPUT_EMAIL).clear();
+  cy.get(INPUT_EMAIL).type("example@gmail.com");
 
   // Phone number input
-  cy.get(":nth-child(3) > #input").type("123456789");
+  cy.get(INPUT_PHONENUMBER).type("123456789");
 
-  cy.get(BACKGROUND_NEXT_BUTTON).click();
+  cy.get(NEXT_BUTTON).click();
 }
 
 function InitCypress() {
@@ -46,14 +50,14 @@ function InitCypress() {
 }
 
 describe("Next step", () => {
-  it("should show goBack button when click next button and should hidden goBack when step is 1", () => {
+  it("should show goBack button when click next button and should hidden goBack button when step is 1", () => {
     InitCypress();
 
     ClickOnNextButtonAndVerifyIfGoBackExists();
     ClickOnGoBackButtonAndVerifyIfInStepOneItsHidden();
   });
 
-  it("should to detect error in fields", () => {
+  it("should verify if form is working", () => {
     InitCypress();
 
     VerifyIfFormIsWorking();
